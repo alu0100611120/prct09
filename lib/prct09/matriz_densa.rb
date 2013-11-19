dc789c7228e6f739ae7821317ecb8a17a0180c9a
Cabaleiro <cabaleiro8@gmail.com>
2013-11-19 17:02:11 +0000

    Estructura inicial de matriz_densa.rb

---
+++ lib/prct09/matriz_densa.rb
@@ -0,0 +1,11 @@
+require 'matriz.rb'
+
+class Matriz_densa < Matriz
+
+	attr_accessor :m
+
+        def initialize (matriz)
+               raise ArgumentError, 'El parametro debe ser una matriz' unless matriz.is_a? Array
+               @m = matriz
+        end
+end
