28d042a061ce648684147d281950edd0b0b41449
Cabaleiro <cabaleiro8@gmail.com>
2013-11-20 11:16:34 +0000

    Primeras pruebas

--- spec/prct09_spec.rb
+++ spec/prct09_spec.rb
@@ -1,11 +1,19 @@
 require 'rspec'
-require 'matriz.rb'
-require 'matriz_dispersa.rb'
-require 'matriz_densa.rb'
+require 'prct09/matriz.rb'
+require 'prct09/matriz_dispersa.rb'
+require 'prct09/matriz_densa.rb'
 
 describe Matriz do
 	before :each do
 		#Inicializar variables
+		@mden = Matriz_densa.new([[1,1,1],[1,1,1],[1,1,1]])
+		@mdis = Matriz_dispersa.new 0 => {0 => 1}, 1 => {1 => 1}, 2 => {2 => 1} #Diagonal de 1
 	end
 	#Sentencias it
+	it "Se debe almacenar una matriz densa" do
+		@mden.class.should == Matriz_densa
+	end
+	it "Se debe almacenar una matriz dispersa" do
+		@mdis.class.should == Matriz_dispersa
+	end
 end
