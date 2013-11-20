0dccafc02785270feae5ef9f1e4af732e0287cb8
Cabaleiro <cabaleiro8@gmail.com>
2013-11-20 10:46:47 +0000

    Estructura spec

---
+++ spec/prct09_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'matriz.rb'
+require 'matriz_dispersa.rb'
+require 'matriz_densa.rb'
+
+describe Matriz do
+	before :each do
+		#Inicializar variables
+	end
+	#Sentencias it
+end
