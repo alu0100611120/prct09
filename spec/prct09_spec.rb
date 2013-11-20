fbb71323144442c165efb8a8f6522bbb690d4771
Cabaleiro <cabaleiro8@gmail.com>
2013-11-20 11:21:50 +0000

    Pequeño cambio para enriquecer las variables del spec

--- spec/prct09_spec.rb
+++ spec/prct09_spec.rb
@@ -6,8 +6,8 @@ require 'prct09/matriz_densa.rb'
 describe Matriz do
 	before :each do
 		#Inicializar variables
-		@mden = Matriz_densa.new([[1,1,1],[1,1,1],[1,1,1]])
-		@mdis = Matriz_dispersa.new 0 => {0 => 1}, 1 => {1 => 1}, 2 => {2 => 1} #Diagonal de 1
+		@mden = Matriz_densa.new([[0,1,2],[1,2,3],[3,4,5]])
+		@mdis = Matriz_dispersa.new 0 => {0 => 1}, 1 => {1 => 2}, 2 => {2 => 3} #Diagonal de 1
 	end
 	#Sentencias it
 	it "Se debe almacenar una matriz densa" do
